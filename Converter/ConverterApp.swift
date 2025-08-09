88357e1c93ad655dfa77cd15dfd7c62020ddf9f5
syamprasaddokka <syamprasaddokka@gmail.com>
2025-08-09 07:12:21 +0530

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Dokka Syam Prasad on 09/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
